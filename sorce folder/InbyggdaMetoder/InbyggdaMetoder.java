e4b8e7f96131b71d026248d6efed4be2073df646
hussein421 <hussein90090@outlook.com>
2018-11-30 15:30:45 +0100

    gjorde 2/4

---
+++ sorce folder/InbyggdaMetoder/InbyggdaMetoder.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class InbyggdaMetoder {
+		
+	static double KlotVolym(double r) {
+		double volym = 4*Math.PI*Math.pow(r,3);
+		return volym;
+	}
+	static void reverse(String str) {
+		Scanner input= new 
+	     String reverse= 
+	        
+	}
+	static int count(String str, char c) {
+		
+	}
+}
---
+++ sorce folder/InbyggdaMetoder/Main.java
@@ -0,0 +1,12 @@
+package InbyggdaMetoder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		InbyggdaMetoder sc= new InbyggdaMetoder();
+		
+		
+		System.out.println(sc.Double(12, 15));
+	}
+
+}
