62a94a7be8d8f1de9166dcf37981fcc2025f8016
hussein421 <hussein90090@outlook.com>
2019-03-04 10:00:28 +0100

    kommentera så att jag vet vad dom olika kodsnutterna gör

--- gissaTal/gissaTal.java
+++ gissaTal/gissaTal.java
@@ -5,7 +5,7 @@ public class gissaTal {
 
 	public static void main(String[] args) {
 		boolean playing = true;
-		int antal = 0;
+		int antal = 0;//antal gissingar
 		Scanner input = new Scanner(System.in);
 		int Nummret = 0;
 		int detNummretPersonenSkriver = 0;
@@ -13,17 +13,18 @@ public class gissaTal {
 		while (playing) {
 
 			
-			if (antal == 0) {
-				Nummret = (int) (Math.random() * 10 + 1);
+			if (antal == 0) 
+				{// den börjar om antal gissningar är 0
+				Nummret = (int) (Math.random() * 10 + 1);//gissning numret
 				System.out.println("gissa på ett tal mellan 1-10");
 			}
-			antal++;// antal försök
-			boolean guessing =true;
+			antal++;// antal gissningar går upp varje gång
+			boolean guessing = true;
 			while(guessing) {
 				try {
 					detNummretPersonenSkriver = input.nextInt(); // personens input
 					guessing = false;
-				} catch (InputMismatchException e) {
+				} catch (InputMismatchException e) { // ifall det inte är ett nummer så säger den till att fösöka om istället för att crasha 
 					guessing = true;
 					System.out.println("Det är inte ett tal, du måste gissa på ett tal!");
 					input.nextLine();
@@ -31,24 +32,24 @@ public class gissaTal {
 			}
 			
 			
-			if ( detNummretPersonenSkriver < 1 || detNummretPersonenSkriver > 10  ){
+			if ( detNummretPersonenSkriver < 1 || detNummretPersonenSkriver > 10  ){ // om man gissar ett nummer över 10 eller mindre än 1 så kommer det säga att det inte är ett giltigt giss
 				System.out.println( detNummretPersonenSkriver + " Är inte ett giltligt giss, försök igen!");
-			} else if (detNummretPersonenSkriver > Nummret) {
+			} else if (detNummretPersonenSkriver > Nummret) { // gissen är mindre än talet
 				System.out.println("mitt tal är lägre, försök igen!");
 
-			} else if (detNummretPersonenSkriver < Nummret) {
+			} else if (detNummretPersonenSkriver < Nummret) { // gissen är högre än talet
 				System.out.println("mitt tal är högre, försök igen!");
 			}  else {
-				System.out.println("Rätt, det tog dig " + antal + " försök dock");
+				System.out.println("Rätt, det tog dig " + antal + " försök dock"); // när man gissar rätt
 				System.out.println("vill du spela igen? i så fall skrev ja");
 
 				String buffer = input.nextLine();// den skippar denna linejn för nån annledning
 				String froga = input.nextLine();
 				if (froga.equals("ja")) {
-					antal = 0;
-					playing = true;
-				} else {
-					System.out.println("okej hejdå");
+					antal = 0; // antal försök resetar 
+					playing = true; // kör om spelet
+				} else { // om man inte vill spela igen
+					System.out.println("okej hejdå"); 
 					break;
 				}
 
@@ -56,4 +57,5 @@ public class gissaTal {
 		}
 
 	}
+
 }
