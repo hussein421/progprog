63677abe25e3b59a5947227cf7053deff4a14297
hussein421 <hussein90090@outlook.com>
2019-03-18 10:32:16 +0100

    klar med uppgiften
    gjorde en extra array lista för det som händer alltså en logg

--- SecureDoors/upgft.java
+++ SecureDoors/upgft.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class upgft {
 	public static void main(String[] args) {
 		Scanner i = new Scanner(System.in);
-		ArrayList<String> secureDoors = new ArrayList<>();
+		ArrayList<String> namesInBuilding = new ArrayList<>();// vilka som är inne och inte inne
+		ArrayList<String> entryLog = new ArrayList<>();//vad som händer
 		boolean playing = true;
 		int times = i.nextInt();// hur många gånger du vill köra
 		i.nextLine(); // buffer
@@ -14,22 +15,28 @@ public class upgft {
 				String choiceAndName = i.nextLine(); // du skriver entry och namnet eller exit och namnet
 				if (choiceAndName.contains("entry")) {
 					String nameOnly = choiceAndName.substring(6, (choiceAndName.length()));
-						if (secureDoors.contains(nameOnly)) {// räknar bort dom 5 första bokstäverna
-						secureDoors.add(nameOnly + " entered (anomaly)");
+						if (namesInBuilding.contains(nameOnly)) {// räknar bort dom 6 första bokstäverna
+						namesInBuilding.add(nameOnly);
+						entryLog.add(nameOnly + " entered (anomaly)");
 					} else {
-						secureDoors.add(nameOnly + " entered");
+						namesInBuilding.add(nameOnly);
+						entryLog.add(nameOnly + " entered");
 					}
 				} else if (choiceAndName.contains("exit")) { // hans ska ut
-					String nameOnly = choiceAndName.substring(5, (choiceAndName.length()));
-					if (secureDoors.contains(nameOnly)) {
-						secureDoors.add(nameOnly + " exit");// han var inne och nu gick ut
+					String nameOnly = choiceAndName.substring(5, (choiceAndName.length()));// räknar bort dom 5 första bokstäverna alltså exit och space
+					if (namesInBuilding.contains(nameOnly)) {
+						namesInBuilding.remove(nameOnly); 
+						entryLog.add(nameOnly + " exit");// han var inne och nu gick ut
 					} else {
-						secureDoors.add(nameOnly + " exit (anomaly)");
+						 namesInBuilding.remove(nameOnly);
+						entryLog.add(nameOnly + " exit (anomaly)");
 					}
 				}
 			}
 			playing = false;
-			System.out.println(secureDoors);
+			for(String logItem : entryLog) {
+				System.out.println(logItem);
+			}
 		}
 	}
 }
