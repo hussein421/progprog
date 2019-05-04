70b93fcf24cd5af28e6c16cde1360acc80a550aa
hussein421 <hussein90090@outlook.com>
2019-05-04 18:25:28 +0200

    la till hälsningar för att fina upp spelet

--- Hangman/hangmann2.java
+++ Hangman/hangmann2.java
@@ -4,19 +4,38 @@ import java.util.Scanner;
 import com.sun.xml.internal.fastinfoset.algorithm.BuiltInEncodingAlgorithm.WordListener;
 
 public class hangmann2 {
+
 	
-	static public void gissa(String sjalva_ordet, int liv ) {// omvandlar ordet till -
+	public static void main(String[] args) { // main metoden 
+		
+		ArrayList<String> nameList= new ArrayList<>();// orden man skall gissa på 
+		nameList.add("samar");
+		nameList.add("samira");
+		nameList.add("maryam");
+		nameList.add("haiwan");
+		nameList.add("kelb");
+		int liv=5;
+		String sjalva_ordet = nameList.get((int)(Math.random()*4)); // själva ordet personen skall gissa på slumpas fram
+		
+		
+		gissa(sjalva_ordet, liv);// anropar metoden
+		
+	}
+	
+	static public void gissa(String sjalva_ordet, int liv ) {// hela själva metoden 
 		char[] gissa_ordet = new char [sjalva_ordet.length()];
 		int i=0;
-		while(i<sjalva_ordet.length()) {
+		while(i<sjalva_ordet.length()) { // omvandlar ordet till ---
 			gissa_ordet[i]='-';
 			if(sjalva_ordet.charAt(i) == (' ')) {
 				gissa_ordet[i]=' ';
 			}
 			i++;
 		}
-		System.out.print(gissa_ordet);
-		System.out.println("          liv kvar:"+liv);
+				System.out.println("hej och välkomen till mitt hänga gubbe spel!");
+				System.out.println("du har så här många liv kvar:" + liv);
+				System.out.print(gissa_ordet);
+		
 		
 		Scanner input = new Scanner(System.in); // så att den kan läsa charachtarer		
 
@@ -88,18 +107,6 @@ public class hangmann2 {
 	}
 	
 	
-	public static void main(String[] args) {
-	
-		ArrayList<String> nameList= new ArrayList<>();// orden man skall gissa på 
-		nameList.add("samar");
-		nameList.add("samira");
-		nameList.add("maryam");
-		nameList.add("haiwan");
-		nameList.add("kelb");
-		int liv=5;
-		String sjalva_ordet = nameList.get((int)(Math.random()*4)); // själva ordet personen skall gissa på slumpas fram
-		gissa(sjalva_ordet, liv);// anropar metoden
-		
-	}
+
 
 }
