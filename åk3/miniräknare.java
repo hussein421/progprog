62d28744c40512d4b5b101afaa6c70a5e6080631
hussein421 <hussein90090@outlook.com>
2019-10-09 11:33:28 +0200

    mini
    asdsd

---
+++ åk3/miniräknare.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class miniräknare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Button one = new Button();
+			
+			
+	}
+
+}
