efb95a40d995e23afe1a4b15053e7edee1bd243a
hussein421 <hussein90090@outlook.com>
2019-10-17 13:52:18 +0200

    skrev av det du skrev på tavlan

--- åk3/miniräknare.java
+++ åk3/miniräknare.java
@@ -1,9 +1,14 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -11,8 +16,13 @@ import javafx.stage.Stage;
 import java.util.ArrayList; 
 
 public class miniräknare extends Application implements EventHandler<ActionEvent>{
-	ArrayList<Character> knapparna = new ArrayList<Character>();
-
+	ArrayList<Button> knapparna = new ArrayList<Button>();
+	TextField field = new TextField();	
+	GridPane gridPane = new GridPane();  
+	Button likamed = new Button("=");
+	
+	
+	
 	public static void main(String[] args) {
 	
 		launch(args);
@@ -20,15 +30,53 @@ public class miniräknare extends Application implements EventHandler<ActionEvent
 
 	@Override
 	public void start(Stage primaryStage){
-			
-
+		
+		knappar();
+		knapparPlats();
+		gridPane.setTranslateY(30);
+		gridPane.setTranslateX(-150);
+		likamed.setTranslateX(-130);
+		likamed.setTranslateY(140);
+		
+		
+		primaryStage.setTitle("calculator");
+		HBox box = new HBox(field,gridPane,likamed);
+		  
+		Scene scene = new Scene(box, 300, 300);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 		
 		
 	}
+	
+	public void knapparPlats() {
+		int buttonPlats = 0;
+		for(int rad = 0; rad <5; rad++) {
+			for(int column = 0; column < 3; column++) {
+				gridPane.add(knapparna.get(buttonPlats), column, rad);
+				buttonPlats++;
+				if(buttonPlats == knapparna.size())
+					break;
+			}
+		}
+	}
+	public void grid() {
+		
+	}
 	public void knappar(){
-		knapparna.add('0');	knapparna.add('X');	knapparna.add('/');	knapparna.add('=');knapparna.add('-');knapparna.add('+');knapparna.add('9');
-		knapparna.add('1');knapparna.add('8');knapparna.add('7');knapparna.add('6');knapparna.add('5');knapparna.add('4');knapparna.add('3');
-		knapparna.add('2');
+		char[] karaktärer = new char[] {
+			'0', '1','2','3','4','5','6','7','8','9','+','-','X','/',','
+		};
+		
+		for(char key : karaktärer) {
+			String keyText = key + "";
+			Button tempButton = new Button(keyText);
+			tempButton.setId(keyText);
+			tempButton.setOnAction(event ->{
+				field.textProperty().set(field.textProperty().get()+keyText);
+			});
+			knapparna.add(tempButton);
+		}
 		
 		
 		
